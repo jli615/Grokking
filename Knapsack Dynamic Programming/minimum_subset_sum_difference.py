80afb7a4ff9431dc76e1ec2290f4eb75397c770e
Justin Li <jlisd04@gmail.com>
2023-06-19 20:55:16 -0700

    minimum subset sum difference

---
+++ Knapsack Dynamic Programming/minimum_subset_sum_difference.py
@@ -0,0 +1,131 @@
+"""MY CODe"""
+
+def can_partition(num):
+  # TODO: Write your code here
+  array_sum = sum(num)
+  desired_sum = array_sum // 2
+
+  dp = [[False for i in range(desired_sum + 1)] for j in range(len(num))]
+
+  for i in range(len(num)):
+    dp[i][0] = True
+  
+  for j in range(1, desired_sum + 1):
+    dp[0][j] = num[0] == j
+
+  for i in range(1, len(num)):
+    for j in range(1, desired_sum + 1):
+      if dp[i-1][j]:
+        dp[i][j] = True
+      else:
+        if j >= num[i]:
+          dp[i][j] = dp[i-1][j-num[i]]
+
+  end_array = desired_sum
+  while dp[-1][end_array] == False:
+    end_array -= 1
+  
+  return array_sum - 2 * end_array
+
+
+  return -1
+
+
+def main():
+  print("Can partition: " + str(can_partition([1, 2, 3, 9])))
+  print("Can partition: " + str(can_partition([1, 2, 7, 1, 5])))
+  print("Can partition: " + str(can_partition([1, 3, 100, 4])))
+
+
+main()
+
+"""SOLUTION - top down
+
+def can_partition(num):
+  s = sum(num)
+  dp = [[-1 for x in range(s+1)] for y in range(len(num))]
+  return can_partition_recursive(dp, num, 0, 0, 0)
+
+
+def can_partition_recursive(dp, num, currentIndex, sum1, sum2):
+  # base check
+  if currentIndex == len(num):
+    return abs(sum1 - sum2)
+
+  # check if we have not already processed similar problem
+  if dp[currentIndex][sum1] == -1:
+    # recursive call after including the number at the currentIndex in the first set
+    diff1 = can_partition_recursive(
+      dp, num, currentIndex + 1, sum1 + num[currentIndex], sum2)
+
+    # recursive call after including the number at the currentIndex in the second set
+    diff2 = can_partition_recursive(
+      dp, num, currentIndex + 1, sum1, sum2 + num[currentIndex])
+
+    dp[currentIndex][sum1] = min(diff1, diff2)
+
+  return dp[currentIndex][sum1]
+
+
+def main():
+  print("Can partition: " + str(can_partition([1, 2, 3, 9])))
+  print("Can partition: " + str(can_partition([1, 2, 7, 1, 5])))
+  print("Can partition: " + str(can_partition([1, 3, 100, 4])))
+
+
+main()
+
+
+SOLUTION - bottom up
+
+
+def can_partition(num):
+  s = sum(num)
+  n = len(num)
+  dp = [[False for x in range(int(s/2)+1)] for y in range(n)]
+
+  # populate the s=0 columns, as we can always form '0' sum with an empty set
+  for i in range(0, n):
+    dp[i][0] = True
+
+  # with only one number, we can form a subset only when the required sum is equal to that number
+  for j in range(0, int(s/2)+1):
+    dp[0][j] = num[0] == j
+
+  # process all subsets for all sums
+  for i in range(1, n):
+    for j in range(1, int(s/2)+1):
+      # if we can get the sum 's' without the number at index 'i'
+      if dp[i - 1][j]:
+        dp[i][j] = dp[i - 1][j]
+      elif j >= num[i]:
+        # else include the number and see if we can find a subset to get the remaining sum
+        dp[i][j] = dp[i - 1][j - num[i]]
+
+  sum1 = 0
+  # find the largest index in the last row which is true
+  for i in range(int(s/2), -1, -1):
+    if dp[n - 1][i]:
+      sum1 = i
+      break
+
+  sum2 = s - sum1
+  return abs(sum2 - sum1)
+
+
+def main():
+  print("Can partition: " + str(can_partition([1, 2, 3, 9])))
+  print("Can partition: " + str(can_partition([1, 2, 7, 1, 5])))
+  print("Can partition: " + str(can_partition([1, 3, 100, 4])))
+
+
+main()
+
+
+
+
+
+
+
+
+"""
